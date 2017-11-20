0ce5f9dcd4fcb648ea7d5c1670c1bb2a322d4764
silvia-YQY <545067444@qq.com>
2017-11-20 23:02:14 +0800

    jQuery ajax

--- ajax/test.js
+++ ajax/test.js
@@ -1,6 +1,6 @@
 var http = require("http")
 var server = http.createServer(function(req,res){
-	res.setHeader('Content-Type','text/html; charset="utf-8"')
+	res.setHeader('Content-Type','text/html; "charset","utf-8"')
     res.end()
 })
 
